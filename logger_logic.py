3a03faf6afd281524dc16a452a18fbe76567782c
gorg <gorg8100@gmail.com>
2026-06-21 19:55:14 +0300

    first commit

---
+++ logger_logic.py
@@ -0,0 +1,41 @@
+import functools
+from settings import DATA_LOGS_PATH
+from typing import TypeVar, Callable, Literal
+from typing_extensions import ParamSpec
+from datetime import datetime
+
+
+def write_log(record_type: Literal["Error", "RepeatError"], error: Exception, func_name: str):
+    with open(DATA_LOGS_PATH, "a") as file:
+        print(
+            f"[{datetime.now().replace(microsecond=0)}][{record_type}]{type(error).__name__}: {error}. In {func_name}.",
+            file=file)
+
+
+F_Spec = ParamSpec("F_Spec")
+F_Return = TypeVar("F_Return")
+
+
+def logg(repeat: int = 1) -> Callable[[Callable[F_Spec, F_Return]], Callable[F_Spec, F_Return]]:
+    if repeat < 1:
+        raise RuntimeError('Repeat must be greater than 0')
+
+    def decorator_logg(func: Callable[F_Spec, F_Return]) -> Callable[F_Spec, F_Return]:
+        @functools.wraps(func)
+        def wrapper(*args: F_Spec.args, **kwargs: F_Spec.kwargs) -> F_Return:
+            error = None
+            for i in range(repeat):
+                try:
+                    original_result = func(*args, **kwargs)
+                    return original_result
+                except Exception as err:
+                    if i != repeat - 1:
+                        write_log(record_type="RepeatError", error=err, func_name=func.__name__)
+                    else:
+                        error = err
+            write_log(record_type="Error", error=error, func_name=func.__name__)
+            raise error
+
+        return wrapper
+
+    return decorator_logg
---
+++ main.py
@@ -0,0 +1,31 @@
+import urllib.request
+from logger_logic import logg
+from settings import SUBSCRIPTION_URL, USER_AGENT, HWID, DEVICE_OS, VER_OS, DEVICE_MODEL, DATA_FILE_PATH
+
+
+@logg()
+def write_subscription_data(text) -> None:
+    with open(DATA_FILE_PATH, "w", encoding="utf-8") as file:
+        file.write(text)
+
+
+@logg(5)
+def get_subscription_data() -> str:
+    req = urllib.request.Request(SUBSCRIPTION_URL)
+    req.add_header('User-Agent', USER_AGENT)
+    req.add_header('x-hwid', HWID)
+    req.add_header('x-device-os', DEVICE_OS)
+    req.add_header('x-ver-os', VER_OS)
+    req.add_header('x-device-model', DEVICE_MODEL)
+    # req.add_header('Accept', '*/*')
+    response = urllib.request.urlopen(req)
+    text = response.read()
+    return text.decode('utf-8')
+
+
+def main():
+    write_subscription_data(get_subscription_data())
+
+
+if __name__ == "__main__":
+    main()
